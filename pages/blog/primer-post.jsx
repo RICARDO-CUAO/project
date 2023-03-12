8f5139f12905d77d66d1f48e0ca97181387f62c9
RicardoCuao <cuaoricardo@gmail.com>
2023-03-12 07:15:30 -0400

    new file:   components/Layout.jsx new file:   pages/about.jsx new file:   pages/blog/[id].jsx new file:   pages/blog/index.jsx modified:   pages/blog/primer-post.jsx new file:   pages/contact.jsx deleted:    pages/index.js new file:   pages/index.jsx new file:   public/images/1.jpg new file:   styles/Layout.module.css new file:   styles/utils.module.css

--- pages/blog/primer-post.jsx
+++ pages/blog/primer-post.jsx
@@ -1,19 +1,9 @@
-import Link from "next/link";
-import Image from "next/image";
-import Head from "next/image";
+import Layout from "../../components/Layout";
 
 export default function primerPost() {
   return (
-    <div>
-      <Head></Head>
+    <Layout>
       <h1>Mi Primer Post</h1>
-      <Image
-        src="/img/1.jpg"
-        width={600}
-        height={600}
-        alt="Mi imagen conimage"
-      ></Image>
-      <Link href="/">Volver al Inicio</Link>
-    </div>
+    </Layout>
   );
 }
